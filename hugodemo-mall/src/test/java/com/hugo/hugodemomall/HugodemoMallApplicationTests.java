103865dab0da966a931124f42ace9c1c3c5dced9
Hugo <test@gmail.com>
2024-04-01 22:39:49 +0800

    初始化 Spring Boot project

---
+++ hugodemo-mall/src/test/java/com/hugo/hugodemomall/HugodemoMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.hugodemomall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HugodemoMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
